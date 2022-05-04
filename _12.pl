19014bebe88c7d8cf0b2f03b2be7e43b68ed1b53
Tsinana <hlebusru@gmail.com>
2022-05-04 12:25:39 +0300

    Update _12.pl
    Сделал _13

--- _12.pl
+++ _12.pl
@@ -19,6 +19,10 @@ min_div_digit(1,1):-!.
 min_div_digit(0,_):- write('Duralei'),!,fail.
 min_div_digit(A,Div):- min_div_digit(A,Div,2).
 
+%lenght_digit(+A,-X)
+lenght_digit(0,0):- !.
+lenght_digit(A,X):- A1 is A div 10,lenght_digit(A1,X1),X is X1 +1.
+
 %11
 %func_11(+A,?Count)
 rec_func_11_d(A,C,A,X):-!, C is X.
@@ -42,13 +46,15 @@ func_12(A,X,Div,I):- not(nod(A,I,1)),not(0 is (I mod Div)),!,
 func_12(A,X,Div,I):- I1 is I - 1, func_12(A,X,Div,I1).
 func_12(A,X):- min_div_digit(A,Div),I is A -1,func_12(A,X1,Div,I),sum_all_digit(A,Y),X is X1 * Y.
 
-% 13. Cкажем, что n - цифровое число является пандигитальным, если оно
-% использует все цифры от 1 до n ровно один раз. Например, 2143 - это
-% 4-значный пандигитал, а также простое число. Какое самое большое n-
-% цифровое пандигитальное простое число существует? Задача должна быть
-% решена без использования списков. Самое большое число n(n-1)(n-2)..1
-
-
+% 13
+%func_13(+A,-X)
+func_13(0,XX,_,XX):-!.
+func_13(N,X,L,XX):- N1 is N-1,lenght_digit(N1,L1),L1 is L,!,
+    pow(10,L,Y),XX1 is (N+XX)*Y,func_13(N1,X,L1,XX1).
+func_13(N,X,L,XX):- N1 is N-1,L1 is L-1, pow(10,L1,Y),XX1 is (N+XX)*Y,func_13(N1,X,L1,XX1).
+func_13(N,X,L,XX):- N1 is N-1,
+    XX1 is (N+XX),func_13(N1,X,L,XX1).
+func_13(N,X):-lenght_digit(N,L), func_13(N,X,L,0).
 
 
 
