8c9887006bbc4baa16e548fc48c02ddc19e5125d
Tsinana <hlebusru@gmail.com>
2022-05-17 10:47:12 +0300

    15 - done
    Tри подруги вышли в белом, зеленом и синем платьях и туфлях. Известно, что только у Ани цвета платья и туфлей совпадали. Ни туфли,
    ни платье Вали не были белыми. Наташа была в зеленых туфлях. Определить
    цвета платья и туфель на каждой из подруг.

--- LB_13.pl
+++ LB_13.pl
@@ -101,9 +101,9 @@ el_no([_|Tail],Num,Ind,El):-Ind1 is Ind+1,el_no(Tail,Num,Ind1,El).
 
 % 14
 pr_commun:- Friends=[_,_,_],
-in_list(Friends,[blond,_]),
-in_list(Friends,[brunt,_]),
-in_list(Friends,[orange,_]),
+el_no(Friends,1,[blond,_]),
+el_no(Friends,2,[brunt,_]),
+el_no(Friends,3,[orange,_]),
 in_list(Friends,[_,rishow]),
 in_list(Friends,[_,below]),
 in_list(Friends,[_,chernov]),
@@ -111,3 +111,20 @@ in_list(Friends,[_,chernov]),
 (in_list(Friends,[brunt,rishow]);in_list(Friends,[brunt,below])),
 (in_list(Friends,[orange,below]);in_list(Friends,[orange,chernov])),
 write(Friends).
+
+% 15
+per_15:- List=[_,_,_],
+    el_no(List,1,[aAnna,_,_]),
+    el_no(List,2,[vValia,_,_]),
+    el_no(List,3,[nNatasha,_,_]),
+    in_list(List,[_,_,white]),
+    in_list(List,[_,_,green]),
+    in_list(List,[_,_,blue]),
+    in_list(List,[_,white,_]),
+    in_list(List,[_,green,_]),
+    in_list(List,[_,blue,_]),
+    not(in_list(List,[vValia,white,white])),
+    in_list(List,[nNatasha,_,green]),
+    in_list(List,[aAnna,A,A]),
+    write(List).
+
